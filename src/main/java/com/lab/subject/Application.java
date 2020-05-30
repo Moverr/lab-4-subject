da98549c91eca730018c168b6bd647eb11c25001
Muyinda ROgers <moverr@gmail.com>
2020-05-31 01:06:50 +0300

    init

---
+++ src/main/java/com/lab/subject/Application.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.lab.subject;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ *
+ * @author muyin
+ */
+public class Application {
+     SpringApplication.run(Application.class, args);
+}
